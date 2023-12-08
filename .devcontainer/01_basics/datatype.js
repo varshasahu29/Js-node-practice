a16e5e2de95b62eec8d0d588681baaf2a2f5d0e8
varshasahu29 <101543116+varshasahu29@users.noreply.github.com>
2023-12-08 10:49:57 +0000

    datatype.js done

---
+++ .devcontainer/01_basics/datatype.js
@@ -0,0 +1,19 @@
+// we are using Node.js not browser
+numver 
+// bigint = bahut bada number ho jab rial case pr use hota hai
+// string => ""
+// boolean : true/false
+// null => standalone value
+// undefind => mtlb ki value ayi nhi hai
+// symbol = uniquesness find krne ke liye use hota hai
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack ( Primitive ), Heep (Non-Primitive) Reference 
+let myname = "varsha";
+let anothername = myname;
+ anothername ="megha";
+
+console.log(myname);
+console.log(anothername);
